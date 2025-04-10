6237a567e14e19de83b732a54ed0ff746fdab062
mxmlnwbr <maximilian.weber@bluewin.ch>
2025-04-10 15:01:45 +0200

    Remove redundant Swiss webcam locations and reorder remaining entries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -164,20 +164,6 @@ const webcams: Webcam[] = [
   {
     name: "Flüelen, Gruonbach",
     location: "Switzerland",
-    image: "https://www.windsurfing-urnersee.ch/webcam/webcam.jpg",
-    windSpeed: "8-12 knots",
-    lastUpdated: "Just now",
-    isLive: true,
-    views: [
-      {
-        name: "Main View",
-        image: "https://www.windsurfing-urnersee.ch/webcam/webcam.jpg",
-      },
-    ],
-  },
-  {
-    name: "Sisikon, Surf Club",
-    location: "Switzerland",
     image: "https://elbeato.bplaced.net/webcamSurfclub/webcam_bucht.jpg",
     windSpeed: "10-15 knots",
     lastUpdated: "Just now",
@@ -190,20 +176,6 @@ const webcams: Webcam[] = [
       {
         name: "Axenegg View",
         image: "https://elbeato.bplaced.net/webcamSurfclub/webcam_axenegg.jpg",
-      },
-    ],
-  },
-  {
-    name: "Oberaegeri",
-    location: "Segel Club Aegeri",
-    image: "https://vcdn.bergfex.at/webcams/archive.new/downsized/4/23544/2025/04/10/23544_2025-04-10_1445_688d47e0ed941b8b.jpg",
-    windSpeed: "8-12 knots",
-    lastUpdated: "Just now",
-    isLive: true,
-    views: [
-      {
-        name: "Main View",
-        image: "https://vcdn.bergfex.at/webcams/archive.new/downsized/4/23544/2025/04/10/23544_2025-04-10_1445_688d47e0ed941b8b.jpg",
       },
     ],
   },
@@ -222,6 +194,20 @@ const webcams: Webcam[] = [
       {
         name: "South View",
         image: "/api/proxy?url=http://bhsboots.myhostpoint.ch/kamera05.jpg",
+      },
+    ],
+  },
+  {
+    name: "Oberaegeri",
+    location: "Segel Club Aegeri",
+    image: "https://vcdn.bergfex.at/webcams/archive.new/downsized/4/23544/2025/04/10/23544_2025-04-10_1445_688d47e0ed941b8b.jpg",
+    windSpeed: "8-12 knots",
+    lastUpdated: "Just now",
+    isLive: true,
+    views: [
+      {
+        name: "Main View",
+        image: "https://vcdn.bergfex.at/webcams/archive.new/downsized/4/23544/2025/04/10/23544_2025-04-10_1445_688d47e0ed941b8b.jpg",
       },
     ],
   },
